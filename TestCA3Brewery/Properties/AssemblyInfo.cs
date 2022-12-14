68a29c6f2591c633d0b749b0779249bd925aea04
Elijel <x00161872@mytudublin.ie>
2022-12-14 17:22:14 +0000

    Added Unit Test

---
+++ TestCA3Brewery/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCA3Brewery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCA3Brewery")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56405f8c-1350-41a4-9f4f-e8360e37c2c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestCA3Brewery/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using CA3Brewery.Brewery;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CA3Brewery.Tests
+{
+    [TestClass]
+    public class BreweryServiceTests
+    {
+        private BreweryService _breweryService;
+        private HttpClient _httpClient;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Create a new HttpClient for making HTTP requests
+            _httpClient = new HttpClient();
+
+            // Create a new instance of the BreweryService using the HttpClient
+            _breweryService = new BreweryService(_httpClient);
+        }
+
+        [TestMethod]
+        public async Task SearchBreweries_ReturnsBreweriesForQuery()
+        {
+            var query = "Brewery";
+
+            var result = await _breweryService.SearchBreweries(query);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count > 0);
+            Assert.IsTrue(result.Any(b => b.Name.Contains(query, StringComparison.OrdinalIgnoreCase)));
+        }
+    }
+}
